a0b698fc2835d2f853b78cd2c9f4b86369a47c9c
JoshuaViera <joshuaviera95@gmail.com>
2018-10-05 16:45:24 -0400

    first commit/Practice

---
+++ practice000/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  practice000
+//
+//  Created by Joshua Viera on 10/5/18.
+//  Copyright © 2018 Joshua Viera. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
